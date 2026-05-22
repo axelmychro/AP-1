37c0d79977bdd3dc0dd65e107b61fa0d241c15ac
Axel <axelmychro@gmail.com>
2026-05-22 10:54:33 +0700

    init latihan 5

---
+++ review-post-uts/05-pointer-tukar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void tukar(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{
+	int a = 10, b = 20;
+	cout << "a=" << a << "; b=" << b << endl;
+	
+	tukar(&a, &b);
+	cout << "a=" << a << "; b=" << b << endl;
+	
+	return 0;
+}
